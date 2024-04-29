dc78555fc002eed23b77b647a12b1524b9ae3ee5
rodrigo745 <roodrigoquinteros@gmail.com>
2024-04-29 11:03:14 -0300

    first commit

---
+++ app/pages/payment/route.ts
@@ -0,0 +1,9 @@
+import type { NextRequest } from "next/server";
+
+export async function POST(request: NextRequest ){
+    const body: unknown = await request.json();
+
+    console.log("body:", body)
+    return Response.json({success: true});
+}
+
